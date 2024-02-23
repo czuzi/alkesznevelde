6a65c0fc6b385976988bf092ec30b6347d53e000
czuzi <you@example.com>
2024-02-23 09:02:43 +0100

    iteration over recursion, delete cache

--- main.py
+++ main.py
@@ -9,54 +9,55 @@ from src.business.affray_page import getAffrayPage
 from src.business.affray.protection_money import collectProtectionMoney, endCollectProtectionMoney
 
 def protectionMoney():
-    businessPage = getBusinessPage().text
+    while True:
+        businessPage = getBusinessPage().text
 
-    print(businessPage)
+        energyString = '<td><b>Energiád:</b></td><td style="text-align:right;">'
 
-    energyString = '<td><b>Energiád:</b></td><td style="text-align:right;">'
+        energyIndex = businessPage.find(energyString) + len(energyString)
 
-    energyIndex = businessPage.find(energyString) + len(energyString)
+        energy = int(businessPage[energyIndex:energyIndex + 2])
 
-    energy = int(businessPage[energyIndex:energyIndex + 2])
+        if businessPage.find('A fogdába azok kerülnek, akiknek nem sikerült a Garázdálkodás!') > -1:
+            print("prison")
+            time.sleep(10)
 
-    if businessPage.find('A fogdába azok kerülnek, akiknek nem sikerült a Garázdálkodás!') > -1:
-        print("prison")
-        time.sleep(10)
-        protectionMoney()
+        elif businessPage.find('A sötét utat választottad: garázdálkodsz!') > -1:
+            print("already making money")
+            time.sleep(30)
 
-    elif energy >= 15 and businessPage.find('A fogdába azok kerülnek, akiknek nem sikerült a Garázdálkodás!') == -1 and businessPage.find('Jelenleg  az igazak álmát alszod') == -1:
-        getAffrayPage()
-        collectProtectionMoney()
-        print("money process started")
-        time.sleep(605)
-        endCollectProtectionMoney()
-        protectionMoney()
+        elif businessPage.find(' kerestél garázdálkodással!') > -1:
+            print("end process")
+            endCollectProtectionMoney()
 
-    elif energy >= 15 and businessPage.find('Jelenleg  az igazak álmát alszod!') > -1:
-        wakeUp()
-        getBusinessPage()
-        getAffrayPage()
-        collectProtectionMoney()
-        print("woke up and money process started")
-        time.sleep(605)
-        endCollectProtectionMoney()
-        protectionMoney()
+        elif energy >= 15 and businessPage.find('A fogdába azok kerülnek, akiknek nem sikerült a Garázdálkodás!') == -1 and businessPage.find('Jelenleg  az igazak álmát alszod') == -1:
+            getAffrayPage()
+            collectProtectionMoney()
+            print("money process started")
+            time.sleep(605)
+            endCollectProtectionMoney()
 
-    elif energy < 15 and businessPage.find('Jelenleg  az igazak álmát alszod!') == -1:
-        getMainPage()
-        sleep()
-        print("low energy, go to sleep")
-        time.sleep(30)
-        protectionMoney()
+        elif energy >= 15 and businessPage.find('Jelenleg  az igazak álmát alszod!') > -1:
+            wakeUp()
+            getBusinessPage()
+            getAffrayPage()
+            collectProtectionMoney()
+            print("woke up and money process started")
+            time.sleep(605)
+            endCollectProtectionMoney()
 
-    elif energy < 15 and businessPage.find('Jelenleg  az igazak álmát alszod!') > -1:
-        print("low energy, sleeping")
-        time.sleep(30)
-        protectionMoney()
+        elif energy < 15 and businessPage.find('Jelenleg  az igazak álmát alszod!') == -1:
+            getMainPage()
+            sleep()
+            print("low energy, go to sleep")
+            time.sleep(30)
 
-    else:
-        print("what else?")
-        time.sleep(30)
-        protectionMoney()
+        elif energy < 15 and businessPage.find('Jelenleg  az igazak álmát alszod!') > -1:
+            print("low energy, sleeping")
+            time.sleep(30)
+
+        else:
+            print("what else?")
+            time.sleep(30)
 
 protectionMoney()
